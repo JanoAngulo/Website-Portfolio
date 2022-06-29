c92e72551f0f50325ec7cf2aa01f9d2c84d42525
Minosuke1219 <93524736+JanoAngulo@users.noreply.github.com>
2022-06-29 16:55:07 +0800

    Added Scrollbar Color

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import './index.css';
+import React from "react";
+import "./index.css";
+import "./App.css"
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
-import Features from './components/Features';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
-import Navbar2 from './components/Navbar';
+import Features from "./components/Features";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+
 
 
 function App() {
 	return (
-		<div className='w-full h-full bg-[#eceff4] '>
-			<div className='sticky top-0 z-[999]'>
-			<Navbar/>
+		<div className="w-full h-full bg-[#eceff4]">
+			<div className="sticky top-0 z-[999]">
+				<Navbar />
 			</div>
 			<Main />
-			<Features/>
-			<Portfolio/>
-			<Resume/>
-			<Contact/>
+			<Features />
+			<Portfolio />
+			<Resume />
+			<Contact />
 		</div>
 	);
 }
 
-export default App
+export default App;
